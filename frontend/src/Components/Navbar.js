cabd10e0f1a4a9c5dfd6af2374430ee433f94534
Vivek23-hub <chaudharivivek858@gmail.com>
2025-09-11 20:51:39 +0530

    basic ui done

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='navdiv'>
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>About</li>
+        </ul>
+    </div>
+    
+  )
+}
+
+export default Navbar
